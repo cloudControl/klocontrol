f9130addc10f5995d2648d591c90676a00ab8e79
Fernando Álvarez <fa@cloudcontrol.de>
2014-12-02 19:31:36 +0100

    Refactor server file

--- config.go
+++ config.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"html/template"
+	"os"
+	"strconv"
 )
 
 var (
 	sensorUrl      = "https://api.spark.io/v1/devices/53ff69066667574832581667/light?access_token=b77ca0da2879ebfa132bd6e8861c09b4a0adc33f"
 	lightThreshold = 1000
 	index          = template.Must(template.ParseFiles("templates/index.html"))
+
+	port string
 )
+
+func SetConfig() {
+	if v := os.Getenv("sensor_url"); v != "" {
+		sensorUrl = v
+	}
+	if v := os.Getenv("light_threshold"); v != "" {
+		if newValue, err := strconv.Atoi(v); err == nil {
+			lightThreshold = newValue
+		}
+	}
+}
--- server.go
+++ server.go
@@ -4,27 +4,23 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"os"
 	"runtime"
-	"strconv"
 )
 
+func init() {
+	flag.StringVar(&port, "port", "8080", "HTTP listen port")
+	SetConfig()
+}
+
 func main() {
-	httpPort := flag.String("port", "8080", "HTTP listen port")
 	flag.Parse()
 
-	if v := os.Getenv("sensor_url"); v != "" {
-		sensorUrl = v
-	}
-	if v := os.Getenv("light_threshold"); v != "" {
-		if newValue, err := strconv.Atoi(v); err == nil {
-			lightThreshold = newValue
-		}
-	}
+	log.Print("Welcome to klocontrol!")
 	log.Printf("GOMAXPROCS: %v", runtime.GOMAXPROCS(-1))
-	log.Printf("Port: %v", *httpPort)
+	log.Printf("Port: %v", port)
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.HandleFunc("/", indexHandler)
 
-	log.Fatal(http.ListenAndServe(":"+*httpPort, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
